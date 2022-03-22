bfae6ee13653275a49230f2ded987212bbad92ee
smile19439 <smile19439@gmail.com>
2022-03-22 23:18:43 +0800

    redirect by short url

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const ShortUrl = require('../../models/shorturl')
+
 router.get('/', (req, res) => {
   res.render('home')
+})
+
+router.get('/:shortenedUrl', (req, res) => {
+  shortenedUrl = req.params.shortenedUrl
+  return ShortUrl.find({ shortenedUrl })
+    .then((url) => {
+      res.redirect(url[0].originalUrl)
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
