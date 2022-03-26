e169ea4bbbc986373868b850791fb2155a3c1623
smile19439 <smile19439@gmail.com>
2022-03-26 13:41:38 +0800

    fix problem of originalUrl undefined

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
-const shortened = require('./modules/shortened')
 
 router.use('/', home)
-router.use('/shortened' , shortened)
 
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,9 +2,48 @@ const express = require('express')
 const router = express.Router()
 
 const ShortUrl = require('../../models/shorturl')
+const getShortenedUrl = require('../../models/getShortenedUrl')
 
 router.get('/', (req, res) => {
   res.render('home')
+})
+
+router.post('/', (req, res) => {
+  const originalUrl = req.body.originalUrl
+  let ShortenedUrlArray = []
+  let shortenedUrl = ''
+  let randomString = ''
+
+  return ShortUrl.find()
+    .lean()
+    .then((url) => {
+      url.forEach((element) => {
+        if (element.originalUrl === originalUrl) {
+          // 如果輸入網址相同會產生相同短網址
+          shortenedUrl = `${req.headers.origin}/${element.shortenedUrl}`
+        } else {
+          // 把資料庫短網址存成陣列
+          ShortenedUrlArray = getShortenedUrl.ShortenedUrlToArray(element)
+        }
+      })
+      
+      if (shortenedUrl.length = 0) {
+        // 產生隨機字串，如果與已產生字串相同則重新產生
+        randomString = getShortenedUrl()
+        while (ShortenedUrlArray.includes(randomString)) {
+          randomString = getShortenedUrl()
+        }
+        shortenedUrl = `${req.headers.origin}/${randomString}`
+      }
+      res.render('home', { shortenedUrl })
+      return randomString
+    })
+    .then((randomString) => {
+      if (randomString) {
+        return ShortUrl.create({ originalUrl, shortenedUrl: randomString })
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 router.get('/:shortenedUrl', (req, res) => {
